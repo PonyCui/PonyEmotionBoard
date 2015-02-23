38708884e7a9560badfdbabb7082c99262a1bf0a
ponycui <PonyCui_Home@saitekiiMac.local>
2015-02-23 09:43:44 +0800

    Initial Commit

---
+++ PonyEmotionBoard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PonyEmotionBoard
+//
+//  Created by 崔 明辉 on 15-2-23.
+//  Copyright (c) 2015年 崔 明辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ PonyEmotionBoardTests/PonyEmotionBoardTests.m
@@ -0,0 +1,40 @@
+//
+//  PonyEmotionBoardTests.m
+//  PonyEmotionBoardTests
+//
+//  Created by 崔 明辉 on 15-2-23.
+//  Copyright (c) 2015年 崔 明辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PonyEmotionBoardTests : XCTestCase
+
+@end
+
+@implementation PonyEmotionBoardTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
